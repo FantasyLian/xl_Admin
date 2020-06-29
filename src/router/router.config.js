071953b14d3449db1c5883bff7687551bff0a363
Mr.cat <fantasy.lian@163.com>
2020-06-29 23:58:07 +0800

    修改路由

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 import Index from '@/components/Index'
 import Login from '@/components/Login'
@@ -38,199 +38,199 @@ import TongsType from '@/components/statistics/RingtoneType'
 import RedPacket from '@/components/statistics/RedPacket'
 
 const routes = [
-    {
-        path: '/',
-        component: Index,
-        redirect: 'ringtones/list',
-        name: 'ringtones',
-        children: [
-            {
-                path: '/ringtones/list',
-                component: RingtoneList,
-                meta: {
-                    title: '炫铃列表'
-                }
-            }, {
-                path: '/ringtones/type',
-                component: RingType,
-                meta: {
-                    title: '炫铃类型'
-                }
-            }, {
-                path: '/ringtones/reason',
-                component: AuditingReason,
-                meta: {
-                    title: '审核理由'
-                }
-            }, {
-                path: '/ringtones/quantity',
-                component: UserQuantity,
-                meta: {
-                    title: '铃声数量'
-                }
-            }, {
-                path: '/ringtones/label',
-                component: Labels,
-                meta: {
-                    title: '标签管理'
-                }
-            }, {
-                path: '/ringtones/joke',
-                component: JokeList,
-                meta: {
-                    title: '段子管理'
-                }
-            }
-        ]
-    },
-    {
-        path: '/users',
-        component: Index,
-        redirect: 'users/list',
-        name: 'users',
-        children: [
-            {
-                path: '/users/list',
-                component: UserList,
-                meta: {
-                    title: '用户列表'
-                }
-            }, {
-                path: '/users/system',
-                component: SystemUser,
-                meta: {
-                    title: '系统用户'
-                }
-            }, {
-                path: '/users/complain',
-                component: UserComplain,
-                meta: {
-                    title: '用户投诉'
-                }
-            }, {
-                path: '/users/chat',
-                component: ChatHistory,
-                meta: {
-                    title: '聊天记录'
-                }
-            }, {
-                path: '/users/bills',
-                component: BillsList,
-                meta: {
-                    title: '流水账单'
-                }
-            }, {
-                path: '/users/problem',
-                component: ProblemUser,
-                meta: {
-                    title: '问题用户'
-                }
-            }, {
-                path: '/users/history',
-                component: HistoryUser,
-                meta: {
-                    title: '历史用户'
-                }
-            }, {
-                path: '/users/selfbuilt',
-                component: SelfbuiltUser,
-                meta: {
-                    title: '自建用户'
-                }
-            }
-        ]
-    },
-    {
-        path: '/friends',
-        component: Index,
-        redirect: 'friends/dynamic',
-        name: 'friends',
-        children: [
-            {
-                path: '/friends/dynamic',
-                component: Dynamic,
-                meta: {
-                    title: '动态管理'
-                }
-            }, {
-                path: '/friends/feedback',
-                component: Feedback,
-                meta: {
-                    title: '用户反馈'
-                }
-            }
-        ]
-    },
-    {
-        path: '/developers',
-        component: Index,
-        redirect: 'developers/noticetype',
-        name: 'develpoers',
-        children: [
-            {
-                path: '/developers/noticetype',
-                component: NoticeType,
-                meta: {
-                    title: '通知类型'
-                }
-            }, {
-                path: '/developers/smsnotify',
-                component: SmsNotify,
-                meta: {
-                    title: '短信通知'
-                }
-            }
-        ]
-    },
-    {
-        path: '/statistics',
-        component: Index,
-        redirect: 'statistics/userdata',
-        name: 'statistics',
-        children: [
-            {
-                path: '/statistics/userdata',
-                component: UserData,
-                meta: {
-                    title: '用户数据'
-                }
-            }, {
-                path: '/statistics/tonesdata',
-                component: RingtoneData,
-                meta: {
-                    title: '铃声数据'
-                }
-            }, {
-                path: '/statistics/finance',
-                component: Finance,
-                meta: {
-                    title: '财务统计'
-                }
-            }, {
-                path: '/statistics/tonestype',
-                component: TongsType,
-                meta: {
-                    title: '铃声类型'
-                }
-            }, {
-                path: '/statistics/redpacket',
-                component: RedPacket,
-                meta: {
-                    title: '红包派发金额'
-                }
-            }
-        ]
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login
-    },
-    {
-        path: '*',
-        component: NotFound,
-        hidden: true
-    }
+  {
+    path: '/',
+    component: Index,
+    redirect: 'ringtones/list',
+    name: 'ringtones',
+    children: [
+      {
+        path: '/ringtones/list',
+        component: RingtoneList,
+        meta: {
+          title: '炫铃列表'
+        }
+      }, {
+        path: '/ringtones/type',
+        component: RingType,
+        meta: {
+          title: '炫铃类型'
+        }
+      }, {
+        path: '/ringtones/reason',
+        component: AuditingReason,
+        meta: {
+          title: '审核理由'
+        }
+      }, {
+        path: '/ringtones/quantity',
+        component: UserQuantity,
+        meta: {
+          title: '铃声数量'
+        }
+      }, {
+        path: '/ringtones/label',
+        component: Labels,
+        meta: {
+          title: '标签管理'
+        }
+      }, {
+        path: '/ringtones/joke',
+        component: JokeList,
+        meta: {
+          title: '段子管理'
+        }
+      }
+    ]
+  },
+  {
+    path: '/users',
+    component: Index,
+    redirect: 'users/list',
+    name: 'users',
+    children: [
+      {
+        path: '/users/list',
+        component: UserList,
+        meta: {
+          title: '用户列表'
+        }
+      }, {
+        path: '/users/system',
+        component: SystemUser,
+        meta: {
+          title: '系统用户'
+        }
+      }, {
+        path: '/users/complain',
+        component: UserComplain,
+        meta: {
+          title: '用户投诉'
+        }
+      }, {
+        path: '/users/chat',
+        component: ChatHistory,
+        meta: {
+          title: '聊天记录'
+        }
+      }, {
+        path: '/users/bills',
+        component: BillsList,
+        meta: {
+          title: '流水账单'
+        }
+      }, {
+        path: '/users/problem',
+        component: ProblemUser,
+        meta: {
+          title: '问题用户'
+        }
+      }, {
+        path: '/users/history',
+        component: HistoryUser,
+        meta: {
+          title: '历史用户'
+        }
+      }, {
+        path: '/users/selfbuilt',
+        component: SelfbuiltUser,
+        meta: {
+          title: '自建用户'
+        }
+      }
+    ]
+  },
+  {
+    path: '/friends',
+    component: Index,
+    redirect: 'friends/dynamic',
+    name: 'friends',
+    children: [
+      {
+        path: '/friends/dynamic',
+        component: Dynamic,
+        meta: {
+          title: '动态管理'
+        }
+      }, {
+        path: '/friends/feedback',
+        component: Feedback,
+        meta: {
+          title: '用户反馈'
+        }
+      }
+    ]
+  },
+  {
+    path: '/developers',
+    component: Index,
+    redirect: 'developers/noticetype',
+    name: 'develpoers',
+    children: [
+      {
+        path: '/developers/noticetype',
+        component: NoticeType,
+        meta: {
+          title: '通知类型'
+        }
+      }, {
+        path: '/developers/smsnotify',
+        component: SmsNotify,
+        meta: {
+          title: '短信通知'
+        }
+      }
+    ]
+  },
+  {
+    path: '/statistics',
+    component: Index,
+    redirect: 'statistics/userdata',
+    name: 'statistics',
+    children: [
+      {
+        path: '/statistics/userdata',
+        component: UserData,
+        meta: {
+          title: '用户数据'
+        }
+      }, {
+        path: '/statistics/tonesdata',
+        component: RingtoneData,
+        meta: {
+          title: '铃声数据'
+        }
+      }, {
+        path: '/statistics/finance',
+        component: Finance,
+        meta: {
+          title: '财务统计'
+        }
+      }, {
+        path: '/statistics/tonestype',
+        component: TongsType,
+        meta: {
+          title: '铃声类型'
+        }
+      }, {
+        path: '/statistics/redpacket',
+        component: RedPacket,
+        meta: {
+          title: '红包派发金额'
+        }
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '*',
+    component: NotFound,
+    hidden: true
+  }
 ]
 
-export default routes;
+export default routes
